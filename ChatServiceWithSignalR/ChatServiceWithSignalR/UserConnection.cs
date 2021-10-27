8d10bd0a2f4bd3cb063a5094ed1926bc1141f806
Trevor Mearns <spiral272@gmail.com>
2021-10-27 15:11:54 +0900

    first commit

---
+++ ChatServiceWithSignalR/ChatServiceWithSignalR/UserConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServiceWithSignalR
+{
+    public class UserConnection
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+
+    }
+}
